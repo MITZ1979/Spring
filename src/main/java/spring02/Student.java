4a7bee87aca9b8ad41ffb1d91e3ac7a3df1b0ba6
zhangguoGit <2676656856@qq.com>
2017-11-06 15:41:45 +0800

    Spring学习总结（一）——Spring实现IoC的多种方式之使用XML配置的方式实现IOC

---
+++ src/main/java/spring02/Student.java
@@ -0,0 +1,32 @@
+package spring02;
+
+import java.io.File;
+
+/**学生*/
+public class Student extends Person {
+    /**身高*/
+    public int height;
+    /**有参构造方法*/
+    public Student(String name,int height){
+        this.name=name;
+        this.height=height;
+    }
+    /**有参构造方法*/
+    public Student(String name,int height,Address address){
+        this.name=name;
+        this.height=height;
+        this.address=address;
+    }
+    @Override
+    public String toString() {
+        return "Student{" + "height=" + height+",name="+name +'}'+address;
+    }
+
+    public void init(){
+        System.out.println("对象被创建");
+    }
+    public void over(){
+        System.out.println("对象被回收");
+    }
+
+}
