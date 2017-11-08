096573c61fe92b5818ac13e323b73367ba2e7af4
zhangguoGit <2676656856@qq.com>
2017-11-08 10:05:22 +0800

    Spring学习总结（二）——AOP之静态代理1_张果

---
+++ src/main/java/spring_aop_22/TestMath.java
@@ -0,0 +1,12 @@
+package spring_aop_22;
+
+public class TestMath {
+    public static void main(String[] args) {
+        IMath math=new Proxy();
+        int n1=999,n2=112;
+        math.add(n1,n2);
+        math.sub(n1,n2);
+        math.mul(n1,n2);
+        math.div(n1,n2);
+    }
+}
